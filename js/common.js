81a6b57fe306edaffc15998ad077657719fef03c
Artur <morozarturv@gmail.com>
2012-12-04 17:42:02 +0200

    new version

--- js/common.js
+++ js/common.js
@@ -1,51 +1,86 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-//enter social
-$('.enter__fb, .enter__vk').click(function() {
-    $('.popup-bg, .popup').fadeIn(300);
-});
-$('.popup__close, .popup-bg').click(function() {
-    $('.popup-bg, .popup').fadeOut(300);
-});
+//bg
+$('.bgbtn').click(function() {
+	$('.bg').show();
+});	
 
-//$('.tabs__content:not(:first)').hide();
-$('.tabs__link li').click(function() {
-	$('.tabs__link li').removeClass('active');
-	data_item = $(this).attr('class');
-	$('.tabs__content').hide();
-	$('#'+data_item).show();
+//filter radio
+$('.filter__price label').click(function() {
+	$('.filter__price label').removeClass('active');
 	$(this).addClass('active');
 });
 
-$('.tabs__slider').scrollable({
-	prev: '.tabs__slider-prev',
-	next: '.tabs__slider-next'
+//filter range
+$('.range__slider').each(function() {
+  $(this).empty().slider({
+  	range: "max",
+	  min: 0,
+	  max: 5,
+	  value: 0,
+	  slide: function( event, ui ) {
+		 	switch (ui.value) {
+		    case 1:
+		      $(this).parent().next().next().css('width', '28px');
+	  			$(this).parent().next().val(ui.value);
+		      break
+		    case 2:
+		      $(this).parent().next().next().css('width', '58px');
+	  			$(this).parent().next().val(ui.value);
+		      break
+		    case 3:
+		      $(this).parent().next().next().css('width', '88px');
+	  			$(this).parent().next().val(ui.value);
+		      break
+		    case 4:
+		      $(this).parent().next().next().css('width', '118px');
+	  			$(this).parent().next().val(ui.value);
+		      break
+		    case 5:
+		      $(this).parent().next().next().css('width', '148px');
+	  			$(this).parent().next().val(ui.value);
+		      break
+			};
+		}
+  });
 });
 
-//gallery
-$('.gallery__img-s .items a').click(function() {
-    $('.gallery__img-s .items a').removeClass('active');
-    pic_link = $(this).attr('href');
-    $('gallery__img-b img').attr('src',pic_link);
-    $(this).addClass('active');
-    return false;
+//catalog
+$('.catalog__list a').click(function() {
+	$('.catalog__cars').hide();
+	var cc_val = $(this).attr('href');
+	$('.'+cc_val).show();
+	return false;
 });
 
-//filter
-$(function() {
-    $( ".filter" ).slider({
-        range: true,
-        min: 0,
-        max: 100000,
-        values: [ 0, 50000 ],
-        slide: function( event, ui ) {
-        	$("#min").val( ui.values[ 0 ]);
-    		$("#max").val( ui.values[ 1 ]);
-            //$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        }
-    });
-    $("#min").val($( ".filter" ).slider( "values", 0 ));
-    $("#max").val($( ".filter" ).slider( "values", 1 ));
+//top tabs
+$('.top__list a').click(function() {
+	$('.top__list a').removeClass('active');
+	$('.top__slider').fadeOut(500);
+	$(this).addClass('active');	
+	var tl_val = $(this).attr('href');
+	$('.'+tl_val).fadeIn(500);
+	return false;
+});
+
+//top sliders
+$(".top__slider_1").scrollable({
+	items: '.top__items',
+	prev: '.top__slider_1 .top__slider-prev',
+	next: '.top__slider_1 .top__slider-next',
+	circular: true
+});
+$(".top__slider_2").scrollable({
+	items: '.top__items',
+	prev: '.top__slider_2 .top__slider-prev',
+	next: '.top__slider_2 .top__slider-next',
+	circular: true
+});
+$(".top__slider_3").scrollable({
+	items: '.top__items',
+	prev: '.top__slider_3 .top__slider-prev',
+	next: '.top__slider_3 .top__slider-next',
+	circular: true
 });
 
 });
